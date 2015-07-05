593a9bd96ac8d521dc97b470a77b13ebebc4b03b
Morulus <vladimirmorulus@gmail.com>
2015-07-05 16:28:48 +0300

    adds register component

--- index.js
+++ index.js
@@ -1,10 +1,11 @@
-define(['template.html',"abstudio~mrsScroll@0.1.0"], function(html, mrsScroll) {
-	return function() {
-		Synthet.newComponent('triple-aesthetic-columns', html)
-		.on('attached', function(element) {
-			this.$.query('>div>div').each(function(el) {
-				mrsScroll(el);
-			});			
-		});
-	}
+define(['./template.html',"abstudio~synthet@0.0.2", "abstudio~mrsscroll@0.1.0",'./style.css'], function(html, Synthet, mrsScroll) {
+	
+	Synthet.newComponent('triple-aesthetic-columns', html)
+	.on('attached', function(element) {
+		this.$.query('>div>div').forEach(function(el) {
+			mrsScroll(el);
+		});			
+	})
+	.register();
+	
 });
