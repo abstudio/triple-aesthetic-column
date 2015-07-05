186fefda7861982c7d87c404eebe34a76c037a85
Morulus <vladimirmorulus@gmail.com>
2015-07-05 19:59:01 +0300

    require synthet 0.0.4

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-define(['./template.html',"abstudio~synthet@0.0.2", "abstudio~mrsscroll@0.1.0",'./style.css'], function(html, Synthet, mrsScroll) {
+define(['./template.html',"abstudio~synthet@0.0.4", "abstudio~mrsscroll@0.1.1",'./style.css'], function(html, Synthet, mrsScroll) {
 	
 	Synthet.newComponent('triple-aesthetic-columns', html)
 	.on('attached', function(element) {
